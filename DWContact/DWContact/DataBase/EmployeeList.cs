518daa32fb45f2f73c51d1eb05b5eb77c4f1aa4e
Alex <Myasnikovav84@gmail.com>
2018-08-29 19:47:59 +0300

    Добавлен список организаций

--- DWContact/DWContact/DataBase/EmployeeList.cs
+++ DWContact/DWContact/DataBase/EmployeeList.cs
@@ -10,13 +10,6 @@ namespace DWContact
         private static ObservableCollection<Employee> employeesList = new ObservableCollection<Employee>();
         public static ObservableCollection<Employee> Employees { get => employeesList; }
 
-        /*      список ФИО всех сотрудников
-         *      список ФИО по организации
-         *      добавить сотрудника
-         * удалить сотрудник
-         * карточка сотрудника
-         * Обновить данные сотрудника
-         */
 
         /// <summary>
         /// список всех контактов
@@ -32,6 +25,9 @@ namespace DWContact
                             .Where(e => e.Company.Name == company.Name)
                                   .Select(e => e.ToString()).ToList());
 
+        /// <summary>
+        /// Добавление сотрудника
+        /// </summary>
         public static void Add(Employee employee) => employeesList?.Add(employee);
 
         /// <summary>
@@ -39,12 +35,61 @@ namespace DWContact
         /// </summary>
         public static void Remove(Employee employee) => employeesList?.Remove(employee);
 
+        /// <summary>
+        /// поиск сотрудника по ФИО
+        /// </summary>
         public static Employee FindEmployee (string FIO)
         {
             Employee employee = (Employee)employeesList.Where(e => e.ToString() == FIO);
             return employee != null ? employee : null;
         }
 
+        /// <summary>
+        /// Обновление данных сотрудника
+        /// </summary>
+        public static void UpdateEmployee  (Employee employee
+                    , string name, string middleName, string surname
+                    , string pfone1, string pfone2, string pfone3
+                    , string adress, string pozition, Company company, string info)
+        {
+            int indx = employeesList.IndexOf(employee);
+            employeesList[indx].Name = name;
+            employeesList[indx].MiddleName = middleName;
+            employeesList[indx].Surname = surname;
+            employeesList[indx].Pfone1 = pfone1;
+            employeesList[indx].Pfone2 = pfone2;
+            employeesList[indx].Pfone3 = pfone3;
+            employeesList[indx].Adress = adress;
+            employeesList[indx].Pozition = pozition;
+            employeesList[indx].Company = company;
+            employeesList[indx].Info = info;
+        }
+        /// <summary>
+        /// поиск есть ли сотрудники в этой компании
+        /// </summary>
+        public static bool CheckDeleteCompany (Company company)
+        {
+            return employeesList.Where(e => e.Company == company).Count() > 0 ? true : false;
+}
+            //bool flag = true;
 
+            //for (int i = 0; i < employeesList.Count(); i++)
+            //{
+            //    if (employeesList[i].Company == company)
+            //    {
+            //        flag = false;
+            //        continue;
+            //    }
+            //}
+            //return flag;
+
+        
+        /// <summary>
+        /// удаление компании, и сотрудников
+        /// </summary>
+        public static void DeleteCompany(Company company)
+        {
+
+        }
     }
 }
